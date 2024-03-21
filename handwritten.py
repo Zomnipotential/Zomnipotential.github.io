57b234101be6ba5dceca35effcea19db11d5d963
Zomnipotential <robothos@gmail.com>
2024-03-21 11:16:49 +0100

    added streamlit file

---
+++ handwritten.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Handwritten Digit Recognition", page_icon="🧊", layout="centered", initial_sidebar_state="expanded")
+# ---- HEADER SECTION ----
+st.subheader("Handwritten Digit Recognition")
+st.title("Just take a picture of a digit and let the model predict it for you! 📸")
+st.write("This is a simple web app that recognizes handwritten digits using a Convolutional Neural Network (CNN).")
+st.write("[Learn more >](https://zomnipotential.github.io/)")
